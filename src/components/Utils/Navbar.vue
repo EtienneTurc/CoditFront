63d8f824f538951079a132d6c096c7434177e722
Etienne Turc <etienne.turc@ensta-paristech.fr>
2020-02-23 13:34:40 +0100

    Update Navbar

--- src/components/Utils/Navbar.vue
+++ src/components/Utils/Navbar.vue
@@ -1,36 +1,39 @@
 <template>
 	<v-toolbar class="navbar">
-		<v-toolbar-title class="white--text">
-			<span class="app-name">Codit</span>
-		</v-toolbar-title>
-
-		<v-spacer></v-spacer>
-
-		<v-toolbar-items>
-			<v-btn
-				text
-				class="separate white--text"
-				v-if="user.isAdmin"
-				@click="navigation('/addGroup')"
-			>Nouveau Groupe</v-btn>
-			<v-btn
-				text
-				class="separate white--text"
-				v-if="user.isAdmin"
-				@click="navigation('/addExercise')"
-			>Nouveau problème</v-btn>
-			<v-btn text class="separate white--text" @click="navigation('/groups')">Groupes</v-btn>
-			<v-btn text class="separate white--text" @click="navigation('/exercises')">Problèmes</v-btn>
-			<v-btn text disabled class="separate username" style="color:white !important">
-				<v-icon style="color:white !important">mdi-account</v-icon>
-				{{user.firstName}}
-				<br />
-				{{user.lastName}}
-			</v-btn>
-			<v-btn @click="logout" text class="separate log-out white--text">
-				<v-icon>mdi-logout</v-icon>Déconnexion
-			</v-btn>
-		</v-toolbar-items>
+		<v-row>
+			<v-col md="2" class="v-center">
+				<v-toolbar-title class="v-center">
+					<span class="app-name">Codit</span>
+				</v-toolbar-title>
+			</v-col>
+			<v-col md="10" class="v-center">
+				<span class="items-right v-center">
+					<v-btn
+						text
+						class="separate white--text"
+						v-if="user.isAdmin"
+						@click="navigation('/addGroup')"
+					>Nouveau Groupe</v-btn>
+					<v-btn
+						text
+						class="separate white--text"
+						v-if="user.isAdmin"
+						@click="navigation('/addExercise')"
+					>Nouveau problème</v-btn>
+					<v-btn text class="separate white--text" @click="navigation('/groups')">Groupes</v-btn>
+					<v-btn text class="separate white--text" @click="navigation('/exercises')">Problèmes</v-btn>
+					<!-- <v-btn text disabled class="separate username" style="color:white !important">
+						<v-icon style="color:white !important">mdi-account</v-icon>
+						{{user.firstName}}
+						<br />
+						{{user.lastName}}
+					</v-btn>-->
+					<v-btn @click="logout" text class="separate log-out white--text">
+						<v-icon>mdi-logout</v-icon>Déconnexion
+					</v-btn>
+				</span>
+			</v-col>
+		</v-row>
 	</v-toolbar>
 </template>
 
@@ -69,9 +72,35 @@ export default {
 </script>
 
 <style lang="scss">
+// .items {
+// 	display: flex;
+// 	justify-content: space-between;
+// 	align-content: space-between;
+// 	align-items: space-between;
+// }
+
+.items-left {
+	display: block;
+}
+
+.items-right {
+	display: flex;
+	justify-content: flex-end;
+	width: 100%;
+	flex-wrap: wrap;
+}
+
+.logo {
+	margin-top: 10%;
+	margin-left: 5%;
+}
+
 .app-name {
 	font-size: 2rem !important;
+	color: white;
+	margin-left: 10px;
 }
+
 .navbar {
 	background-color: #0984e3 !important;
 }
@@ -93,7 +122,7 @@ export default {
 	// margin-left: 1em;
 }
 
-.separate {
-	border-left: 0.01rem solid rgb(255, 255, 255, 0.95);
-}
+// .separate {
+// 	border-left: 0.01rem solid rgb(255, 255, 255, 0.95);
+// }
 </style>
